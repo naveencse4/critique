14a77f09c06c0f16412c784d0f851bd9d944df9e
naveen.b <naveen.b@divergentcs.org>
2020-08-15 21:46:37 -0700

    completed billing

--- index.js
+++ index.js
@@ -35,6 +35,16 @@ app.get('/', (req,res) => {
     res.send({ bye: 'buddy' });
 });
 
+if(process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets like main.js file or main.css file
+    app.use(express.static('client/build'));
+    // Express will serve up index.html file if it doesnot recognize the route
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
