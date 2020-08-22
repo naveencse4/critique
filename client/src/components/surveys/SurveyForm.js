769c0359551f5c3c818664da0b4df42feaa3d587
naveen.b <naveen.b@divergentcs.org>
2020-08-22 10:38:01 -0700

    started client surveys

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,32 @@
+//SurveyForm shows a form for a user to add input
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+class SurveyForm extends Component {
+    renderFields() {
+        return(
+            <div>
+                <Field type="text" name="title" label="Survey Title" component={SurveyField}/>
+                <Field type="text" name="subject" label="Subject Line" component={SurveyField}/>
+                <Field type="text" name="body" label="Email Body" component={SurveyField}/>
+                <Field type="text" name="emails" label="Recipients List" component={SurveyField}/>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    {this.renderFields()}
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'surveyForm!'
+})(SurveyForm);
